8c1c99f557522b6e623fbe383908ad69f608cc3b
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-04-19 23:35:40 +0530

    updated vite.config with prod backend url

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === "production";
+
+const proxyTarget = isProduction
+  ? "https://real-estate-backend-qitz.onrender.com/"
+  : "http://localhost:8000";
+
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:8000',
-        secure:false,
-      }
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: proxyTarget,
+        changeOrigin: true,
+        secure: !isProduction,
+      },
+    },
   },
   plugins: [react()],
-})
+});
